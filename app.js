ac2b9e26dbf510875fa460c7feab22b842367432
Brad Wynter <brawyn@Brads-MacBook-Air.local>
2016-06-16 10:35:10 +1000

    Added 3 views and route in the app.js file and created the css file for the navbar

--- app.js
+++ app.js
@@ -8,4 +8,20 @@ db.once('open', function() {
   // we're connected!
 });
 
-console.log('Brad was here!');
+app.set('view engine', 'pug');
+
+app.get('/', function (request, response) {
+  response.render('index', {title: 'Index', message: 'Welcome to the world of Adios'});
+});
+
+app.get('/about', function (request, response) {
+  response.render('about', {title: 'About', about: 'A website developed entirely using Pug and Node.js utilising CRUD'});
+});
+
+app.get('/new', function (request, response) {
+  response.render('new', {title: 'New', message: 'Put new form here!'});
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
