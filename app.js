7fc5921b6d112d8e25e39429b4c6e3f89777865d
lukephelan <luke.phelan@hotmail.com>
2016-06-16 10:33:00 +1000

    Have begun to create the schema

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'pug');
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -9,3 +11,14 @@ db.once('open', function() {
 });
 
 console.log('Brad was here!');
+
+var customerSchema = mongoose.Schema({
+    name: String,
+    address: String,
+    suburb: String,
+    postcode: String,
+    comments: [{ body: String, date: Date }],
+    active: Boolean
+});
+
+var Customer = mongoose.model('Customer', customerSchema);
